59743be106bf0428b4c9e511ed9f3a3bb6022028
Edouard <edouardmeurante@gmail.com>
2025-06-28 00:32:55 -0400

    Maj

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import GageApp from './Gage/GageApp';
 import QuestionApp from './QuestionApp/QuestionApp';
